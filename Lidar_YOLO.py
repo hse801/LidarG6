316c890db8249bce5a505b24819e501367e3caed
hse801 <sarah0.hwang@gmail.com>
2020-08-02 16:40:02 +0900

    object 2개 인식

---
+++ Lidar_YOLO.py
@@ -0,0 +1,215 @@
+from threading import Thread
+import cv2
+import numpy as np
+import time
+import serial
+import math
+from math import atan, pi, floor
+import matplotlib.pyplot as plt
+import math
+from YOLO import startyolo
+
+dist_i = 0
+dist_i2 = 0
+anglecheck = 0
+anglecheck2 = 0
+# dist_sum = 0
+angcheckdone = []
+distcheckdone = []
+angcheckdone2 = []
+distcheckdone2 = []
+num = 0
+num_Mean = 0
+num2 = 0
+num_Mean2 = 0
+
+
+def read_Lidar():
+
+    def plot_lidar(distdict):
+        x = [0 for i in range(360)]
+        y = [0 for i in range(360)]
+        for angle in range(0, 360):
+            x[angle] = distdict[angle] * math.cos(math.radians(angle))
+            y[angle] = distdict[angle] * math.sin(math.radians(angle))
+
+        plt.figure(1)
+        plt.cla()
+        plt.ylim(-3000, 3000)
+        plt.xlim(-3000, 3000)
+        plt.scatter(x, y, c='r', s=8)
+        plt.pause(0.001)
+
+    def _CheckSum(data):
+        try:
+            ocs = _HexArrToDec((data[6], data[7]))
+            LSN = data[1]
+            cs = 0x55AA ^ _HexArrToDec((data[0], data[1])) ^ _HexArrToDec((data[2], data[3])) ^ _HexArrToDec(
+                (data[4], data[5]))
+            for i in range(0, 2 * LSN, 2):
+                cs = cs ^ _HexArrToDec((data[8 + i], data[8 + i + 1]))
+
+            if (cs == ocs):
+                return True
+            else:
+                return False
+        except Exception as e:
+            return False
+
+    def _HexArrToDec(data):
+        littleEndianVal = 0
+        for i in range(0, len(data)):
+            littleEndianVal = littleEndianVal + (data[i] * (256 ** i))
+        return littleEndianVal
+
+    def _AngleCorr(dist):
+        if dist == 0:
+            return 0
+        else:
+            return (atan(21.8 * ((155.3 - dist) / (155.3 * dist))) * (180 / pi))
+
+    def _Calculate(d):
+        #global dist_sum
+        global anglecheckdone
+        global distcheckdone
+        ddict = []
+        LSN = d[1]
+        Angle_fsa = ((_HexArrToDec((d[2], d[3])) >> 1) / 64.0)
+        Angle_lsa = ((_HexArrToDec((d[4], d[5])) >> 1) / 64.0)
+
+        if Angle_fsa < Angle_lsa:
+            Angle_diff = Angle_lsa - Angle_fsa
+        else:
+            Angle_diff = 360 + Angle_lsa - Angle_fsa
+
+
+
+        for i in range(0, 2 * LSN, 2):
+            global dist_i
+            dist_i = _HexArrToDec((d[8 + i], d[8 + i + 1])) / 4
+            #dist_i = 2
+            Angle_i_tmp = ((Angle_diff / float(LSN)) * (i / 2)) + Angle_fsa
+
+            #####3rint(dist_i)
+            if Angle_i_tmp > 360:
+                Angle_i = Angle_i_tmp - 360
+            elif Angle_i_tmp < 0:
+                Angle_i = Angle_i_tmp + 360
+            else:
+                Angle_i = Angle_i_tmp
+
+            Angle_i = Angle_i + _AngleCorr(dist_i)
+            # print(Angle_i)
+            ddict.append((dist_i, Angle_i))
+
+            # angcheckdone = []
+            # distcheckdone = []
+            #if Angle_i >200:
+                # Angle_i - 100 <= anglecheck + 149 <= Angle_i + 100
+            if (Angle_i - 16 <= anglecheck + 149 <= Angle_i + 16) and (164 <= Angle_i <= 204):
+                angcheckdone.append(Angle_i)
+                distcheckdone.append(dist_i)
+            else:
+                angcheckdone.append(0)
+                distcheckdone.append(0)
+            print('Angle check = ', angcheckdone)
+            print('Distance check = ', distcheckdone)
+
+            if (Angle_i - 16 <= anglecheck2 + 149 <= Angle_i + 16) and (164 <= Angle_i <= 204):
+                angcheckdone2.append(Angle_i)
+                distcheckdone2.append(dist_i)
+            else:
+                angcheckdone2.append(0)
+                distcheckdone2.append(0)
+            print('Angle check = ', angcheckdone2)
+            print('Distance check = ', distcheckdone2)
+
+            if i == (LSN - 1) * 2:
+                nonzero_distcheckdone=[float(v) for v in distcheckdone if v > 0]
+                nonzero_distcheckdone2 = [float(v) for v in distcheckdone2 if v > 0]
+
+                #mean_dist = (dist_sum / len(distcheckdone))
+            mean_dist = sum(nonzero_distcheckdone) / len(nonzero_distcheckdone)
+            mean_dist2 = sum(nonzero_distcheckdone2) / len(nonzero_distcheckdone2)
+            print('Distance Mean = ', mean_dist)
+            print('Distance Mean2 = ', mean_dist2)
+
+            global num
+            global num2
+            global num_Mean
+            global num_Mean2
+
+            num += 1
+            num2 += 1
+            num_Mean += mean_dist
+            num_Mean2 += mean_dist2
+            print('num = ', num)
+            print('num2 = ', num2)
+            print('total_mean = ', num_Mean)
+            print('total_mean2 = ', num_Mean2)
+            print('avg_mean =', num_Mean / num)
+            print('avg_mean2 =', num_Mean2 / num2)
+                #print(len(distcheckdone))
+                #print('LSN = ', LSN)
+            # print('ddict = ', ddict)
+            return ddict
+
+    def _Mean(data):
+        length_of_data_without_zero = sum([i != 0 for i in data])
+        if len(data) > 0 and length_of_data_without_zero != 0:
+            #        return int(sum(data)/len(data)) # original By ydlidar
+            return float(sum(data) / length_of_data_without_zero)  # modified for remove zero value
+        return 0
+
+    def code(ser):
+        data1 = ser.read(6000)
+        data2 = data1.split(b"\xaa\x55")[1:-1]
+        distdict = {}
+        for i in range(0, 360):
+            distdict.update({i: []})
+        for i, e in enumerate(data2):
+            try:
+                if e[0] == 0:
+                    if (_CheckSum(e)):
+                        d = _Calculate(e)
+                        for ele in d:
+                            angle = floor(ele[1])
+                            if (angle >= 0 and angle < 360):
+                                distdict[angle].append(ele[0])
+            except Exception as e:
+                pass
+        for i in distdict.keys():
+            distdict[i] = _Mean(distdict[i])
+        yield distdict
+
+    def main():
+        # Open Serial
+        ser = serial.Serial(port='COM13', baudrate=512000)
+        ser.isOpen()
+
+        # Scan start
+        values = bytearray([int('a5', 16), int('60', 16)])
+        ser.write(values)
+
+        for i in range(10):
+            angle_data = code(ser)
+            plot_lidar(next(angle_data))
+
+
+        # Scan End
+        values = bytearray([int('a5', 16), int('65', 16)])
+        ser.write(values)
+
+        # Close Serial
+        ser.close()
+
+    if __name__ == '__main__':
+        main()
+
+
+t1 = Thread(target = startyolo, args =(""))
+t2 = Thread(target = read_Lidar, args =(""))
+
+
+t1.start()
+t2.start()
--- read_Lidar.py
+++ read_Lidar.py
@@ -6,6 +6,7 @@ import threading
 import queue
 from serial_Lidar import Protocol, ReaderThread
 import struct
+import math
 
 
 # 프로토콜
@@ -58,25 +59,75 @@ class LidarG6(rawProtocal):
         #print('PH: ', PH, 'CT: ', CT, 'LSN: ', LSN, 'FSA: ', FSA, 'LSA: ', LSA, 'CS: ', CS)
         print(f'PH = {PH}, CT = {CT}, LSN = {LSN}, FSA = {FSA}, LSA = {LSA}, CS = {CS}') #fstring
         sampling_data = []
-        for i in range(11, len(data), 2):
+        for i in range(11, len(data)-1, 2):
             p = struct.unpack('<H', data[i:i + 2])
-            sampling_data.append(list(p))
+            sampling_data.append(p[0])
         print('Sampling Data = ', sampling_data)
-        # print(len(data))
-        # sampling_data = []
-        # for i in range(11, len(data)):
-        #     sampling_data.append(list(struct.unpack('<H', data[i])))
-        #
-        # print('Sampling Data = ', sampling_data)
+        print('Length of Data = ', len(sampling_data))
+        real_data = []
+        for i in range(len(sampling_data)):
+            real_data.append(sampling_data[i]*0.25)
+        print('Distance= ', real_data)
 
-        # # sampling_data = []
-        # sampling_data = struct.unpack('<H', data[11:])
+        an_fsa = (data[2] >> 1) // 64
+        an_lsa = (data[3] >> 1) // 64
+
+        if an_fsa > an_lsa:
+            an_fsa = 360 - an_fsa
+
+        lsn = data[1] >> 8
+        angle = []
+
+        i = 0
+
+        for i in range(5, len(data)):
+            angle.append(((abs(an_fsa - an_lsa) / (lsn - 1)) * i) + an_fsa)
+
+        ang_correct = []
+
+        for cor in range(len(r4eal_data)):
+            if real_data[cor] == 0:
+                ang_correct.append(0)
+            else:
+                ang_correct.append(math.degrees(math.atan(21.8 * (155.3 - real_data[cor]) / (155.3 * real_data[cor]))))
+
+        an_fsa = angle[0] + ang_correct[0]
+        an_lsa = angle[len(real_data) - 1] + ang_correct[len(real_data) - 1]
+
+        corr_angle = []
+        i = 0
+
+        for i in range(lsn):
+            corr_angle.append(((abs(an_fsa - an_lsa) / (lsn - 1)) * i) + an_fsa)
+
+        print('Angle = ', corr_angle)
+
+        # ang_FSA = (FSA >> 1) / 64
+        # ang_LSA = (LSA >> 1) / 64
+        # angle = []
         #
-        #
-        # # sampling_data, = struct.unpack('<H', data[11:])
-        # print('Sampling Data = ', sampling_data)
+        # for i in range(5, len(sampling_data)):
+        #     angle.append(((abs(ang_FSA - ang_LSA) / (LSN - 1)) * i) + ang_FSA)
 
 
+        # if real_data[0] == 0:
+        #     AngCorrect_i = 0
+        # else:
+        #     AngCorrect_i = math.atan(21.8*(155.3-real_data[0])/(155.3*real_data[0]))
+        #     Angle_FSA = (FSA[0:2] + LSN[1]) / 64 + AngCorrect_i
+        #
+        # if real_data[] == 0:
+        #     AngCorrect_i = 0
+        # else:
+        #     AngCorrect_i = math.atan(21.8*(155.3-real_data[LSN])/(155.3*real_data[LSN]))
+        #     Angle_LSA = (LSA[0:2] + FSA[3]) / 64 + AngCorrect_i
+        #
+        # for i in range(2, LSN - 1):
+        #     if real_data[i] == 0:
+        #         AngCorrect_i = 0
+        #     else:
+        #         AngCorrect_i = math.atan(21.8*(155.3-real_data[i])/(155.3*real_data[i]))
+        #         Angle_i = (Angle_LSA - Angle_FSA) * (i - 1) / (LSN - 1) + Angle_FSA + AngCorrect_i
         return
 
 
